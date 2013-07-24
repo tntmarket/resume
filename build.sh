b9bf3c647488b88e2208baba8686093ded29130d
Dave Lu <dave792321001730003212345@gmail.com>
2013-07-24 14:46:17 -0400

    flag for jade should be -o not -O

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ ln -s ../src/normalize.css normalize.css
 ln -s ../src/entypo.css entypo.css
 ln -s ../font font
 cd ..
-jade -w -O build src/resume.jade &
+jade -w -o build src/resume.jade &
 stylus -w -o build src/style.stylus
